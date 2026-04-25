33513696310b6279ce533846f970385b49e806f0
zachjf9 <140197562+zachjf9@users.noreply.github.com>
2026-04-25 11:08:44 -0400

    first steps

--- Mavs2011.py
+++ Mavs2011.py
@@ -3,4 +3,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
+# streamlit run "/Users/zacharyfalzone/Desktop/SouthernCTUniversity/Spring_2026/Sports_Performance/Mav2011Repo/Mavs2011.py"
+
+# regular season data
+df = pd.read_csv('https://raw.githubusercontent.com/zachjf9/Mav2011Repo/refs/heads/main/MavsRegularSeason.csv')
+
+# playoff data
+df = pd.read_csv('https://raw.githubusercontent.com/zachjf9/Mav2011Repo/refs/heads/main/MavsPlayOffs.csv')
+
+st.title("Mavs 2011 Season Performance Analysis")
+
+
 
